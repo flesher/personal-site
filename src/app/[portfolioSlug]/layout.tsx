6d1256029e7673bca91a83685c2352fb408e3c70
Matt Flesher <matt.flesher@gmail.com>
2024-01-30 20:17:27 -0800

    fix: unlist portfolio pages

---
+++ src/app/[portfolioSlug]/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Roboto_Mono } from "next/font/google";
+import "@/styles/globals.scss";
+
+const robo = Roboto_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Matt Flesher",
+  description: "Personal website of Matt Flesher",
+  robots: "noindex",
+};
+
+function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={robo.className}>{children}</body>
+    </html>
+  );
+}
+
+export default Layout;
